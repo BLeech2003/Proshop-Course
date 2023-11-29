eb1c308d3b4744377a433c1783510b3c6f0fa1b7
BLeech2003 <bianca23may2003@gmail.com>
2023-11-29 13:25:48 +0200

    Section 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import {Container} from "react-bootstrap";
+import Header from "./components/header";
+//import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <h1>ProShop</h1>
-  )
-}
+    <>
+    <Header/>
+    <main className="py-3">
+    <Container>
+    <HomeScreen/>
+    </Container>
+    </main>
+    
+    </>
+  );
+};
 
 export default App

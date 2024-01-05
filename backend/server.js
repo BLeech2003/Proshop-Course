d8303505b716cc3f5924435b459efd286de24499
BLeech2003 <bianca23may2003@gmail.com>
2024-01-05 20:07:41 +0200

    Section 7, Backend auth completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import {notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -8,11 +10,16 @@ import {notFound, errorHandler } from "./middleware/errorMiddleware.js";
 connectDB(); //Connect to mongDB
 const PORT = process.env.PORT;
 const app = express();
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(PORT, () => 
